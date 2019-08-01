ea3e0bda568b9bf4b3de6a2965de886441ac5fda
Lukas Simonis <lsimonis@gmail.com>
2019-08-01 22:39:16 +0000

    some config changes

--- lukassimonis_net/settings.py
+++ lukassimonis_net/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = '/var/www/static'
-STATIC_URL = '/static/'
+STATIC_URL = 'https://static.lukassimonis.net/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static"),
 ]

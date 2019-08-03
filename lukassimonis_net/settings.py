42afbe64f0c5983f3ade304a8050cff64d13ec92
Lukas Simonis <lsimonis@gmail.com>
2019-08-02 20:31:14 -0700

    fixing settings for production db, accidentally upated w dev settings

--- lukassimonis_net/settings.py
+++ lukassimonis_net/settings.py
@@ -91,13 +91,13 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -109,7 +109,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
 
 
 # Password validation
